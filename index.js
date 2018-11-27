ab84479b10f7da766dec7b0ce6db7e5c3dad11b8
Tommi Lepola <lepola.tommi@gmail.com>
2018-11-27 18:03:55 +0200

    Implement multiple methods for different kinds of game events.
    Implemented methods:
    - monitorPlayers(players): Monitors all
    players' health and kills.
    - CTWin(): Called when CTs win the round.
    - TWin(): Called when Ts win the round.
    - ace(): Called when a player scores an ace.

--- index.js
+++ index.js
@@ -17,18 +17,26 @@ const options = {
     host: '192.168.10.52'
 }
 
-let roundOver = false;
+/**
+ * Is the round over or not? Used to avoid duplicate method calls.
+ */
+let roundOver = true;
+
+/**
+ * Has the ace been already called or not? Used to avoid duplicate method calls.
+ */
+let aceCalled = false;
 
 /**
  * To be implemented:
- * [X] Send artnet signal when bomb is planted
- * [X] Send artnet signal when bomb is defused
- * [X] Send artnet signal when bomb has exploded
- * [ ] Send artnet signal when CTs win
- * [ ] Send artnet signal when Ts win
- * [ ] Send artnet signal when a player scores an ace
- * [ ] Send 10 different artnet signals when players die
- * [ ] Send 10 different artnet signals when players spawn
+ * [X] Send artnet signal when bomb is planted - Channel 1
+ * [X] Send artnet signal when bomb is defused - Channel 2
+ * [X] Send artnet signal when bomb has exploded - Channel 3
+ * [X] Send artnet signal when CTs win - Channel 4
+ * [X] Send artnet signal when Ts win - Channel 5
+ * [X] Send artnet signal when a player scores an ace - Channel 6
+ * [/] Send 10 different artnet signals when players die
+ * [ ] Simple HTML controls to test that Artnet works properly.
  * /
 
 /*
@@ -36,18 +44,20 @@ let roundOver = false;
  * in the CS:GO .cfg file (either gamestate_integration_observerspectator.cfg
  * or gamestate_integration_consolesample.cfg)
  */
-const authToken = 'CCWJu64ZV3JHDT8hZc';
+const authTokenPlayer = 'CCWJu64ZV3JHDT8hZc';
+const authTokenObserver = 'Q79v5tcxVQ8u';
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    let eventInfo = '';
+    let body = '';
 
     req.on('data', (data) => {
-        eventInfo += processGameEvents(JSON.parse(data.toString()));
+        body += data;
     });
 
     req.on('end', () => {
+        let eventInfo = processGameEvents(JSON.parse(body));
         if (eventInfo !== '') {
             console.log(eventInfo);
         }
@@ -60,7 +70,7 @@ const server = http.createServer((req, res) => {
  * Processes payloads to parse game events
  *
  * @param {object} data - Payload as JSON object
- * @return {string}
+ * @return {string} - The string and the date and time of the game event if a desired game event was detected
  */
 function processGameEvents(data) {
     // Ignore unauthenticated payloads
@@ -90,20 +100,31 @@ function processGameEvents(data) {
 }
 
 /**
- * Checks that the auth token from the CS:GO server matches the one defined in this software.
+ * Checks that the auth token from the CS:GO observer or player matches the one defined in this software.
  *
  * @param {object} data - The payload from CS:GO observer as a JSON object
  * @return {boolean}
  */
 function isAuthentic(data) {
-    return readProperty(data, 'auth.token') === authToken;
+    let authenticated = false;
+    if (readProperty(data, 'auth.token') === authTokenPlayer || readProperty(data, 'auth.token') === authTokenObserver) {
+        authenticated = true;
+    }
+    return authenticated;
 }
 
+/**
+ * Called if round is over. Checks when the round starts and returns indication of that.
+ * 
+ * @param {Object} data - The payload from CS:GO observer as a JSON object
+ * @return {String} - Indication of going live when going live.
+ */
 function detectGoingLive(data) {
     let output ='';
 
     if (readProperty(data, 'round.phase') === "live") {
         roundOver = false;
+        aceCalled = false;
         output = "Going live!";
     }
 
@@ -118,25 +139,106 @@ function detectGoingLive(data) {
 function detectGameEvent(data) {
     let output = '';
 
-    if (readProperty(data, 'added.round.bomb')) {
-        output += bombPlanted();
-    }
+    if (readProperty(data, 'round.phase') === "over") {
+        output += checkWinningTeam(data);
+        roundOver = true;
+    } else {
+        if (readProperty(data, 'added.round.bomb')) {
+            output += bombPlanted();
+        }
 
-    if (readProperty(data, 'round.bomb') === "defused") {
-        output += bombDefused();
-        roundOver = true;
-    } else if (readProperty(data, 'round.bomb') === "exploded") {
-        output += bombExploded();
-        roundOver = true;
+        if (monitorPlayers(readProperty(data, 'allplayers'))) {
+            output += ace();
+        }
     }
 
     return output;
 }
 
 /**
+ * Checks which team won the round and checks if either bomb defusal or explosion happened as well.
+ * 
+ * @param {String} winningTeam - The team which won.
+ */
+function checkWinningTeam(data) {
+    let output = "";
+    if (readProperty(data, 'round.win_team') === 'CT') {
+        output += CTWin();
+
+        if (readProperty(data, 'round.bomb') === "defused") {
+            output += ", "+bombDefused();
+        }
+    } else {
+        output += TWin();
+
+        if (readProperty(data, 'round.bomb') === "exploded") {
+            output += ", "+bombExploded();
+        }
+    }
+    return output;
+}
+
+/**
+ * Iterates constantly through all players and checks whether a player has made an ace (killed 5 enemies) or
+ * is dead.
+ * 
+ * @param {Object} players - object which consists all the players and their information.
+ * @return {boolean} - true if an ace is found, false if an ace was not found.
+ */
+function monitorPlayers(players) {
+    let aceFound = false;
+
+    Object.keys(players).forEach(function(key) {
+        const player = players[key];
+        if (checkKillsAndHealth(player)) {
+            aceFound = true;
+        }
+    });
+    return aceFound;
+}
+
+/**
+ * Checks the amount of kills and health that a single player has.
+ * 
+ * @param {Object} player - the player object which holds information from a single player.
+ * @return {boolean} - true if an ace was found, false if an ace was not found.
+ */
+function checkKillsAndHealth(player) {
+    let aceFound = false;
+
+    Object.keys(player).forEach(function(key) {
+        const state = player[key];
+        if (state.hasOwnProperty('round_kills')) {
+            const kills = state['round_kills'];
+            if (kills === 5 && !aceCalled) {
+                aceFound = true;
+            }
+        }
+
+        if (state.hasOwnProperty('health')) {
+            if (state['health'] == 0) {
+                playerDead(player);
+            }
+        }
+    })
+
+    return aceFound;
+}
+
+/**
+ * Called if a player dies. Later I will implement a solution to dim out a single light, which is focused on a player,
+ * when the player dies.
+ * 
+ * @param {Object} player - the player object which holds information from a single player.
+ */
+function playerDead(player) {
+    console.log(player['name'] + " is dead.");
+}
+
+/**
  * Sends an artnet signal to channel one of the defined universe when bomb was planted.
  * 
- * @return indication to console that bomb plant was detected.
+ * @return {String} - indication to console that bomb plant was detected.
  */
 function bombPlanted() {    
     let artnet = require('artnet')(options);
@@ -151,7 +253,7 @@ function bombPlanted() {
 /**
  * Sends an artnet signal to channel two of the defined universe when bomb was defused.
  * 
- * @return indication to console that bomb defusal was detected.
+ * @return {String} - indication to console that bomb defusal was detected.
  */
 function bombDefused() {
     let artnet = require('artnet')(options);
@@ -166,7 +268,7 @@ function bombDefused() {
 /**
  * Sends an artnet signal to channel three of the defined universe when bomb was defused.
  * 
- * @return indication to console that bomb explosion was detected.
+ * @return {String} - indication to console that bomb explosion was detected.
  */
 function bombExploded() {
     let artnet = require('artnet')(options);
@@ -179,12 +281,60 @@ function bombExploded() {
 }
 
 /**
+ * Sends an artnet signal to channel four of the defined universe when Counter-Terrorists win.
+ * 
+ * @return {String} - indication to console that CTs victory was detected.
+ */
+function CTWin() {
+    let artnet = require('artnet')(options);
+
+    artnet.set(UNIVERSE, 4, 255, function (err, res) {
+        artnet.close();
+    });
+    
+    return "CTs win";
+}
+
+/**
+ * Sends an artnet signal to channel five of the defined universe when Terrorists win.
+ * 
+ * @return {String} - indication to console that Ts victory was detected.
+ */
+function TWin() {
+    let artnet = require('artnet')(options);
+
+    artnet.set(UNIVERSE, 5, 255, function (err, res) {
+        artnet.close();
+    });
+    
+    return "Ts win";
+}
+
+/**
+ * Sends an artnet signal to channel six of the defined universe when someone has made an ace.
+ * Sets aceCalled to be true to avoid calling ace() method multiple times.
+ * 
+ * @return {String} - indication to console that an ace has bene done.
+ */
+function ace() {
+    let artnet = require('artnet')(options);
+
+    artnet.set(UNIVERSE, 6, 255, function (err, res) {
+        artnet.close();
+    });
+
+    aceCalled = true;
+
+    return "A player has aced.";
+}
+
+/**
  * Helper function to read values under nested paths from objects
  *
  * @param {object} container - Object container
  * @param {string} propertyPath - Path to the property in the container
  *                                separated by dots, e.g. 'map.phase'
- * @return {mixed} Null if the object has no requested property, property value
+ * @return {mixed} - Null if the object has no requested property, property value
  *                 otherwise
  */
 function readProperty(container, propertyPath) {
